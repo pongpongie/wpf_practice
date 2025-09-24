1a167aeeae5f651f16c7d5e2d4d95d66da3dc6b3
sihyunlee <sihyun@vinetel.co.kr>
2025-09-24 17:37:02 +0900

    feat: add RelayCommand, ViewModelBase of utils

---
+++ SeminarMvvmApp/utils/ViewModelBase.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+public class ViewModelBase : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler PropertyChanged;
+    protected void OnPropertyChanged(string propertyName) =>
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+}
