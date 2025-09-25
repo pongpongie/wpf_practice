73c8d3a9c200bfeec2a265eafd7948c778ca25f8
sihyunlee <sihyun@vinetel.co.kr>
2025-09-25 18:01:13 +0900

    refactor: rename and refactor ViewModels

---
+++ SeminarMvvmApp/ViewModels/CounterViewModel.cs
@@ -0,0 +1,21 @@
+﻿using SeminarMvvmApp.Utils;
+
+namespace SeminarMvvmApp.ViewModels
+{
+    public class CounterViewModel : ViewModelBase
+    {
+        private int _count;
+        public int Count
+        {
+            get => _count;
+            set => SetProperty(ref _count, value);
+        }
+        public RelayCommand IncrementCommand { get; }
+        public RelayCommand DecrementCommand { get; }
+        public CounterViewModel()
+        {
+            IncrementCommand = new RelayCommand(_ => Count++);
+            DecrementCommand = new RelayCommand(_ => Count--);
+        }
+    }
+}
