07146c6a7fe46e74b9af1e59a7dc6c08be3927f5
sihyunlee <sihyun@vinetel.co.kr>
2025-09-25 18:01:03 +0900

    refactor: rename and refactor View

---
+++ SeminarMvvmApp/Views/CounterView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace SeminarMvvmApp.Views
+{
+    public partial class CounterView : UserControl
+    {
+        public CounterView()
+        {
+            InitializeComponent();
+        }
+    }
+}
