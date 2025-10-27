f643ff7fa74f59e2bb58b77b86397678eecd359a
sihyunlee <sihyun@vinetel.co.kr>
2025-10-27 18:30:08 +0900

    feat: add NotVineAppGUI.LoginModule

---
+++ NotVineApp project/NotVineApp_GUI/NotVIneAppGUI.LoginModule/ViewModels/LoginFormViewModel.cs
@@ -0,0 +1,36 @@
+﻿using NotVineApp.Common.Utils;
+using System.Windows;
+
+namespace NotVineAppGUI.LoginModule.ViewModels
+{
+    internal class LoginFormViewModel : ViewModelBase
+    {
+        private string _textBoxText = "Login Form";
+
+        public string TextBoxText
+        {
+            get => _textBoxText;
+            set
+            {
+                if (_textBoxText != value)
+                {
+                    _textBoxText = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public LoginFormViewModel()
+        {
+            ClickCommand = new RelayCommand(OnButtonClick);
+        }
+
+        public RelayCommand ClickCommand { get; }
+
+        private void OnButtonClick(object parameter)
+        {
+            TextBoxText = "버튼 클릭으로 텍스트 변경됨";
+        }
+
+    }
+}
---
+++ NotVineApp project/NotVineApp_GUI/NotVIneAppGUI.LoginModule/Views/LoginFormView.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Controls;
+
+namespace NotVineAppGUI.LoginModule.Views
+{
+    /// <summary>
+    /// LoginFormView.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class LoginFormView : UserControl
+    {
+        public LoginFormView()
+        {
+            InitializeComponent();
+            this.DataContext = new ViewModels.LoginFormViewModel();
+        }
+    }
+}
