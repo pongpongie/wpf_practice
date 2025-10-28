d21218d6e47fa296cb0dd222320de5caadf034cc
sihyunlee <sihyun@vinetel.co.kr>
2025-10-28 11:32:13 +0900

    feat: add AuthPageView, MainPageViewModel

---
+++ NotVineApp project/NotVineApp_GUI/NotVineAppGUI.RegionPageView/ViewModels/AuthPageViewModel.cs
@@ -0,0 +1,18 @@
+﻿using NotVineAppGUI.LoginModule.ViewModels;
+using NotVineApp.Common.Utils;
+using System.Windows.Input;
+
+namespace NotVineAppGUI.RegionPageView.ViewModels
+{
+    public class AuthPageViewModel : ViewModelBase
+    {
+        public Object CurrentViewModel { get; set; }
+
+        public ICommand ShowLoginFormCommand { get; set; }
+
+        public AuthPageViewModel()
+        {
+            CurrentViewModel = new LoginFormViewModel();
+        }
+    }
+}
---
+++ NotVineApp project/NotVineApp_GUI/NotVineAppGUI.RegionPageView/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NotVineApp.Common.Utils;
+
+namespace NotVineAppGUI.RegionPageView.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+        private object _currentPageViewModel;
+        public object CurrentPageViewModel
+        {
+            get => _currentPageViewModel;
+            set
+            {
+                if (_currentPageViewModel != value)
+                {
+                    _currentPageViewModel = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public MainPageViewModel()
+        {
+            // 초기 뷰모델 설정 (예: AuthPageViewModel)
+            CurrentPageViewModel = new AuthPageViewModel();
+        }
+    }
+}
