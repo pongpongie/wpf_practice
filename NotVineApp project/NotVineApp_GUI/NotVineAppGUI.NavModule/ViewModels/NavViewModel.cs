75a0f9e26dce8226d032e6fe70b61cbdfddc13db
sihyunlee <sihyun@vinetel.co.kr>
2025-10-29 14:27:37 +0900

    feat: add NavModule

---
+++ NotVineApp project/NotVineApp_GUI/NotVineAppGUI.NavModule/ViewModels/NavViewModel.cs
@@ -0,0 +1,14 @@
+﻿using NotVineApp.Common.Utils;
+using NotVineApp.Common.Services;
+
+namespace NotVineAppGUI.NavModule.ViewModels
+{
+    public class NavViewModel : ViewModelBase
+    {
+        private readonly INavigationService _navigationService;
+        public NavViewModel(INavigationService navigationService)
+        {
+            _navigationService = navigationService;
+        }
+    }
+}
---
+++ NotVineApp project/NotVineApp_GUI/NotVineAppGUI.NavModule/Views/NavView.xaml.cs
@@ -0,0 +1,23 @@
+﻿using NotVineAppGUI.NavModule.ViewModels;
+using NotVineApp.Common.Services;
+
+using System.Windows.Controls;
+
+namespace NotVineAppGUI.NavModule.Views
+{
+    /// <summary>
+    /// NavView.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class NavView : UserControl
+    {
+        public NavView(NavViewModel viewModel)
+        {
+            InitializeComponent();
+            this.DataContext = viewModel;
+        }
+
+        public NavView() : this(ServiceLocator.Instance.Resolve<NavViewModel>())
+        {
+        }
+    }
+}
